5e10408193f2e26a0cd98a3701d7f40e20c46e4d
Max <91089748+maxhack1337@users.noreply.github.com>
2025-01-04 00:05:08 +1000

    4.7.2 Update

--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,4 @@
-console.log('Версия 4.7.1 Release');
+console.log('Версия 4.7.2 Release');
 var accentC = document.getElementById('oldaccent');
 var msgreact = document.getElementById('messagereactions');
 var recentgroups = document.getElementById('recentgroups');
