1a74e9aea19af8caa2c43d523c6993441fabf83e
Max <maxi-2001@mail.ru>
2025-03-10 03:53:07 +0300

    Поддержка карточек плейлистов

--- src/modules/inject/functions/modalVideo/modalVideo.ts
+++ src/modules/inject/functions/modalVideo/modalVideo.ts
@@ -6,10 +6,11 @@ const modalVideo = () => {
 
     if (getLocalValue("videoModal")) {
         document.arrive(`body:not(:has(#video_choose_box)) ._video_item`, { existing: true }, async function (e) {
+            let videoId = e.hasAttribute('data-id') ? e.getAttribute('data-id') : '';
             e.setAttribute('onclick', `
 	event.preventDefault();
 	event.stopPropagation();
-	window.showVideo("` + e.getAttribute('data-id') + `",'0',{autoplay: 1, queue: 0, listId: '', playlistId: ''}, this);`)
+	window.showVideo('${videoId}','0',{autoplay: 1, queue: 0, listId: '', playlistId: ''}, this);`)
         });
 
         document.arrive(`[class^="VideoCardList__videoItem"]:has([class^="vkitVideoCardThumb__thumb"][href^="/video"])`, { existing: true }, async function (e) {
@@ -23,7 +24,24 @@ const modalVideo = () => {
             e.setAttribute('onclick', `
 	event.preventDefault();
 	event.stopPropagation();
-	window.showVideo("` + videoLink + `",'0',{autoplay: 1, queue: 0, listId: '', playlistId: ''}, this);`)
+	window.showVideo('${videoLink}','0',{autoplay: 1, queue: 0, listId: '', playlistId: ''}, this);`)
+        });
+
+        document.arrive(`[class^="VideoCard__thumbLink"][href^="/playlist"][data-playlist-id]`, { existing: true }, async function (e) {
+            let videoId;
+            let pListId;
+            try {
+                videoId = e.getAttribute('data-id');
+                pListId = e.getAttribute('data-playlist-id')
+
+            }
+            catch (error) {
+		
+            }
+            e.setAttribute('onclick', `
+	event.preventDefault();
+	event.stopPropagation();
+	window.showVideo('${videoId}','0',{autoplay: 1, queue: 0, listId: '', playlistId: '${pListId}'}, this);`)
         });
 
         document.arrive(`[class^="vkitVideoCardLayout__card"]:has([class^="vkitVideoCardThumb__thumb"][href^="/video"])`, { existing: true }, async function (e) {
@@ -37,7 +55,7 @@ const modalVideo = () => {
             e.setAttribute('onclick', `
 	event.preventDefault();
 	event.stopPropagation();
-	window.showVideo("` + videoLink + `",'0',{autoplay: 1, queue: 0, listId: '', playlistId: ''}, this);`)
+	window.showVideo('${videoLink}','0',{autoplay: 1, queue: 0, listId: '', playlistId: ''}, this);`)
         });
 
 
@@ -59,7 +77,7 @@ const modalVideo = () => {
             e.setAttribute('onclick', `
 	event.preventDefault();
 	event.stopPropagation();
-	window.showVideo("` + videoLink + `",'0',{autoplay: 1, queue: 0, listId: '', playlistId: '` + plistID + `'}, this);`)
+	window.showVideo('${videoLink}','0',{autoplay: 1, queue: 0, listId: '', playlistId: '${plistID}'}, this);`)
         });
 
         document.arrive(`[class^="vkitVideoPlaylistSideBlockThumb__thumbWrapper"]:has([class^="vkitVideoCardThumb__thumb"][href^="/playlist"])`, { existing: true }, async function (e) {
@@ -80,7 +98,7 @@ const modalVideo = () => {
             e.setAttribute('onclick', `
 	event.preventDefault();
 	event.stopPropagation();
-	window.showVideo("` + videoLink + `",'0',{autoplay: 1, queue: 0, listId: '', playlistId: '` + plistID + `'}, this);`);
+	window.showVideo('${videoLink}','0',{autoplay: 1, queue: 0, listId: '', playlistId: '${plistID}'}, this);`);
         });
 
         document.arrive(`[class^="vkitVideoCardLayout__card"]:has([class^="vkitVideoCardThumb__thumb"][href^="/playlist"])`, { existing: true }, async function (e) {
@@ -101,7 +119,7 @@ const modalVideo = () => {
             e.setAttribute('onclick', `
 	event.preventDefault();
 	event.stopPropagation();
-	window.showVideo("` + videoLink + `",'0',{autoplay: 1, queue: 0, listId: '', playlistId: '` + plistID + `'}, this);`)
+	window.showVideo('${videoLink}','0',{autoplay: 1, queue: 0, listId: '', playlistId: '${plistID}'}, this);`)
         });
     }
 }
