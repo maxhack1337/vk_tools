ad6108bf0da979f85f9441c62686426e2e9258e3
Max <maxi-2001@mail.ru>
2025-03-25 14:38:01 +0300

    Дата регистрации пользователей

--- src/modules/inject/functions/classicalProfile/scripts/getRegDateValue.ts
+++ src/modules/inject/functions/classicalProfile/scripts/getRegDateValue.ts
@@ -4,10 +4,10 @@ const getRegDateValue = async(id:number) => {
         const regDateAlready = Number(localStorage.getItem(`regDate_${id}`));
         if (regDateAlready) return formatRegDate(regDateAlready);
 
-        const foafGet = await fetch(`https://vk.com/foaf.php?id=${id}`);
-        const response = await foafGet.text();
-        const [, regDateReady] =
-          response.match(/ya:created dc:date="(.+?)"/) || [];
+        const foafGet = await fetch(`https://api.vkenhancer.ru/vktools.getUserRegistrationDate?id=${id}`);
+        const response = await foafGet.json();
+        const regDateReady =
+          response.vk_tools_registration_date|| [];
         if (regDateReady) {
           const regDateReadyUNIX = new Date(regDateReady).getTime();
           localStorage.setItem(`regDate_${id}`, regDateReadyUNIX.toString());
