bc1e5a484363a440a8163bc14dd0f3eaaec21511
Max <maxi-2001@mail.ru>
2025-04-22 02:21:06 +0300

    Версия 6.1

--- src/Localization/stringLibrary.tsx
+++ src/Localization/stringLibrary.tsx
@@ -107,7 +107,7 @@ const strings = {
   pseudoTab1: ["Кастомизация акцентов", "Accents customization", "Кастомізація акцентів"],
   pseudoTab2: ["Новый мессенджер", "New messenger", "Новий месенджер"],
   pseudoTab3: ["Настройки", "Settings", "Налаштування"],
-  versionNumber: ["Версия 6.0 Release", "v. 6.0 Release", "Версія 6.0 Release"],
+  versionNumber: ["Версия 6.1 Release", "v. 6.1 Release", "Версія 6.1 Release"],
   whatsNew: ["Что нового?", "What's new?", "Що нового?"],
   hideMessageFooter: ["Скрыть переключатель непрочитанных сообщений", "Hide unread messages switcher", "Приховати перемикач непрочитаних повідомлень"],
   doWideFeed: ["Ширина ленты", "Feed width", "Ширина стрічки"],
