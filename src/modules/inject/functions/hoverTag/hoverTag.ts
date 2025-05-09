8ae79e602effca197ebd5cd32189c432b9aefe8e
Max <maxi-2001@mail.ru>
2025-05-09 17:38:41 +0300

    Фикс удаления стандартного ивент-лисенера в функционале стандартного взаимодействия с тегом

--- src/modules/inject/functions/hoverTag/hoverTag.ts
+++ src/modules/inject/functions/hoverTag/hoverTag.ts
@@ -1,26 +1,23 @@
 const hoverTag = () => {
-    if (localStorage.getItem("isOldHover") === 'true') {
-        const hoverTags = [
-            'a.MessageText__link[href^="/"]',
-            'a.MessageText__link[href*="vk.com"]',
-        ];
-        const joinTags = hoverTags.join(', ');
-        document.arrive(joinTags, { existing: true }, function (e) {
-            let href = e.getAttribute("href");
-            let mentionId = href?.substring(href.lastIndexOf("/") + 1);
+  if (localStorage.getItem("isOldHover") === "true") {
+    const hoverTags = ['a.MessageText__link[href^="/"]', 'a.MessageText__link[href*="vk.com"]'];
+    const joinTags = hoverTags.join(", ");
+    document.arrive(joinTags, { existing: true }, function (e) {
+      let href = e.getAttribute("href");
+      let mentionId = href?.substring(href.lastIndexOf("/") + 1);
 
-            e.setAttribute("href", href || '');
-            e.classList.add("mem_link");
-            e.setAttribute("mention", "");
-            e.setAttribute("mention_id", mentionId || '1');
-            e.removeAttribute("onclick");
-            e.setAttribute(
-                "onclick",
-                `window.open('${href}', '_blank'); return false;`
-            );
-            e.setAttribute("onmouseover", "mentionOver(this)");
-        });
-    }
-}
+      e.setAttribute("href", href || "");
+      e.classList.add("mem_link");
+      e.setAttribute("mention", "");
+      e.setAttribute("mention_id", mentionId || "1");
+      e.addEventListener("click", (ev) => {
+        ev.preventDefault();
+        ev.stopPropagation();
+        window.open(href || "", "_blank");
+      });
+      e.setAttribute("onmouseover", "mentionOver(this)");
+    });
+  }
+};
 
 export default hoverTag;
