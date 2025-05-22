61275bea9a228fa7dcc5f05a6afd0104774e8a2c
Max <maxi-2001@mail.ru>
2025-05-23 02:58:19 +0300

    Небольшой фикс DnD

--- src/modules/inject/functions/classicalProfile/scripts/photoModule/photosLoadModule.ts
+++ src/modules/inject/functions/classicalProfile/scripts/photoModule/photosLoadModule.ts
@@ -150,7 +150,7 @@ const photosLoadModule = () => {
     divDrop.classList.remove("drag-over");
   });
 
-  document.body.addEventListener(
+  divDrop.addEventListener(
     "drop",
     async (e) => {
       e.preventDefault();
