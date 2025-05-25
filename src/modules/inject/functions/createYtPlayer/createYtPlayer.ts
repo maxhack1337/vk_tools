82b1488a913b9cdc74acc89e54039f9d5e1cf899
Max <maxi-2001@mail.ru>
2025-05-25 17:30:42 +0300

    Ютуб плеер показ старый дизайн сообщений

--- src/modules/inject/functions/createYtPlayer/createYtPlayer.ts
+++ src/modules/inject/functions/createYtPlayer/createYtPlayer.ts
@@ -40,20 +40,31 @@ const createYtPlayer = () => {
 
     let oldSel = ".im_msg_media_link";
     document.arrive(oldSel, { existing: true }, (oldLink) => {
-      let videoLink = oldLink.querySelector(".mail_link")?.getAttribute("href") || "";
-      if (videoLink && videoLink !== "") {
-        let videoId = getYouTubeVideoId(videoLink);
-        if (videoId && videoId !== null) {
-          let videoYt = document.createElement("div");
-          videoYt.id = "video_yt";
-          videoYt.tabIndex = -1;
-          videoYt.role = "complementary";
-          videoYt.style.width = "100%";
-          videoYt.style.height = "100%";
-          videoYt.style.outline = "0";
-          videoYt.innerHTML = `<iframe class="video_yt_player" onload="document.getElementById('video_yt')?.focus(); " onmouseover="if (cur && cur.incViews) cur.incViews();" width="100%" height="100%" src="https://www.youtube.com/embed/${videoId}?enablejsapi=1&autoplay=0&start=0&autohide=1&wmode=opaque&showinfo=0&origin=https://vk.com&rel=0&iv_load_policy=3" frameborder="0" allowfullscreen="" allow="autoplay; encrypted-media" id="widget2"></iframe>`;
-          oldLink.replaceWith(videoYt);
-        }
+      let title = oldLink.querySelector(".mail_link__title");
+      let subTitle = oldLink.querySelector(".mail_link__subtitle");
+      let mailLink = oldLink.querySelector(".mail_link");
+      if (title && subTitle) {
+        title.textContent = showYtPlayerHead(vk.lang);
+        subTitle.textContent = showYtPlayerFoot(vk.lang);
+        mailLink?.addEventListener("click", (e) => {
+          e.preventDefault();
+          e.stopImmediatePropagation();
+          let videoLink = oldLink.querySelector(".mail_link")?.getAttribute("href") || "";
+          if (videoLink && videoLink !== "") {
+            let videoId = getYouTubeVideoId(videoLink);
+            if (videoId && videoId !== null) {
+              let videoYt = document.createElement("div");
+              videoYt.id = "video_yt";
+              videoYt.tabIndex = -1;
+              videoYt.role = "complementary";
+              videoYt.style.width = "100%";
+              videoYt.style.height = "100%";
+              videoYt.style.outline = "0";
+              videoYt.innerHTML = `<iframe class="video_yt_player" onload="document.getElementById('video_yt')?.focus(); " onmouseover="if (cur && cur.incViews) cur.incViews();" width="100%" height="100%" src="https://www.youtube.com/embed/${videoId}?enablejsapi=1&autoplay=0&start=0&autohide=1&wmode=opaque&showinfo=0&origin=https://vk.com&rel=0&iv_load_policy=3" frameborder="0" allowfullscreen="" allow="autoplay; encrypted-media" id="widget2"></iframe>`;
+              oldLink.replaceWith(videoYt);
+            }
+          }
+        });
       }
     });
   }
