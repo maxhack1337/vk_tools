63e19a2ffa4cde23fe6a878c34411bd3c8c57371
Max <maxi-2001@mail.ru>
2025-06-01 17:44:44 +0300

    Кавычки стрелочки и дефис в тире

--- src/modules/inject/functions/classicalProfile/scripts/groups/fetchGroups.ts
+++ src/modules/inject/functions/classicalProfile/scripts/groups/fetchGroups.ts
@@ -1,7 +1,7 @@
 const fetchGroups = async () => {
   try {
     let objectGroups: any = {};
-    const groups = await vkApi.api("groups.get", { user_id: cur.oid, filter: cur.oid === vk.id ? "groups,events" : "groups", extended: 1 });
+    const groups = await vkApi.api("groups.get", { user_id: cur.oid, extended: 1 });
     if (groups && groups.items && groups.count > 0) {
       groups.items.forEach((item: any) => {
         objectGroups[item.name] = item.screen_name ? `https://vk.com/${item.screen_name}` : `https://vk.com/club${item.id}`;
