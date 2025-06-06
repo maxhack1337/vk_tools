57c00b999facd555f20e52fe68b48112c5a86dd7
Max <maxi-2001@mail.ru>
2025-06-07 00:55:04 +0300

    Учёл переносы строк при подмене эмодзи, дефиса, кавычек

--- src/modules/inject/functions/newDesign/replaceEmojisWithImages.ts
+++ src/modules/inject/functions/newDesign/replaceEmojisWithImages.ts
@@ -1,24 +1,24 @@
-import { EMOJI_HINTS } from './emojiConstants';
+import { EMOJI_HINTS } from "./emojiConstants";
 
 type Hint = [number, string, string];
 
 const replaceEmojisWithImages = (text: string) => {
-    for (const hint of Object.values(EMOJI_HINTS) as Hint[]) {
-        const emojiTexts = Array.isArray(hint[1]) ? hint[1] : [hint[1]];
+  for (const hint of Object.values(EMOJI_HINTS) as Hint[]) {
+    const emojiTexts = Array.isArray(hint[1]) ? hint[1] : [hint[1]];
 
-        for (const emojiText of emojiTexts) {
-            const regex = new RegExp(`(^|\\s)${escapeRegExp(emojiText)}(\\s|$)`, 'g');
-            text = text.replace(regex, (match, p1, p2) => {
-                return (p1 === '' ? '' : ' ') + hint[2] + (p2 === '' ? '' : ' ');
-            });
-        }
+    for (const emojiText of emojiTexts) {
+      const regex = new RegExp(`(^|\\s)${escapeRegExp(emojiText)}(\\s|$)`, "g");
+      text = text.replace(regex, (match, p1, p2) => {
+        return p1 + hint[2] + p2;
+      });
     }
+  }
 
-    return text;
+  return text;
 };
 
 function escapeRegExp(string: string): string {
-    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 }
 
 export default replaceEmojisWithImages;
