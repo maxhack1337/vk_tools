ee780e03624d1cb1e35b63de245901c4f1034980
Alex <vkenhancer@gmail.com>
2025-06-22 00:14:57 +0300

    base

--- src/modules/inject/functions/oldFeed/attachments/getLinkImageMaxSizeUrl.ts
+++ src/modules/inject/functions/oldFeed/attachments/getLinkImageMaxSizeUrl.ts
@@ -1,46 +1,20 @@
 const getLinkImageMaxSizeUrl = (sizes: any) => {
-    const availableSizes = [
-            "a",
-            "b",
-            "i",
-            "p",
-            "q",
-            "s",
-            "w",
-            "z",
-            "y",
-            "x",
-            "r",
-            "o",
-            "m",
-            "g",
-            "max",
-            "l",
-            "f",
-            "k",
-            "c",
-            "e",
-            "d",
-            "j",
-            "temp",
-            "h",
-            "n",
-    ];
-    let n = null,
+  const availableSizes = ["a", "b", "i", "p", "q", "s", "w", "z", "y", "x", "r", "o", "m", "g", "max", "l", "f", "k", "c", "e", "d", "j", "temp", "h", "n", "base"];
+  let n = null,
     e = 0;
-    let t;
-    for (const curSize of sizes) {
-        t = curSize.type;
-        if (availableSizes.includes(t)) {
-            t = (curSize.width || 0) * (curSize.height || 0);
-            if (t > e || t === 0) {
-                e = t;
-                n = curSize;
-            }
-        }
+  let t;
+  for (const curSize of sizes) {
+    t = curSize.type;
+    if (availableSizes.includes(t)) {
+      t = (curSize.width || 0) * (curSize.height || 0);
+      if (t > e || t === 0) {
+        e = t;
+        n = curSize;
+      }
     }
-    let maxSizer = t[0] || n;
-    return maxSizer.url;
-}
+  }
+  let maxSizer = t[0] || n;
+  return maxSizer.url;
+};
 
 export default getLinkImageMaxSizeUrl;
