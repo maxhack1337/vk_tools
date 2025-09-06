5351e52849b314e1f6e5d779e16419a6248bd1c6
Alex <vkenhancer@gmail.com>
2025-09-06 14:48:52 +0300

    chore: bump version to 6.5.7

--- src/Localization/stringLibrary.tsx
+++ src/Localization/stringLibrary.tsx
@@ -123,7 +123,7 @@ const strings = {
   pseudoTab1: ["Кастомизация акцентов", "Accents customization", "Кастомізація акцентів"],
   pseudoTab2: ["Новый мессенджер", "New messenger", "Новий месенджер"],
   pseudoTab3: ["Настройки", "Settings", "Налаштування"],
-  versionNumber: ["Версия 6.5.6 Release", "v. 6.5.6 Release", "Версія 6.5.6 Release"],
+  versionNumber: ["Версия 6.5.7 Release", "v. 6.5.7 Release", "Версія 6.5.7 Release"],
   whatsNew: ["Что нового?", "What's new?", "Що нового?"],
   hideMessageFooter: ["Скрыть переключатель непрочитанных сообщений", "Hide unread messages switcher", "Приховати перемикач непрочитаних повідомлень"],
   doWideFeed: ["Ширина ленты", "Feed width", "Ширина стрічки"],
